e02aba228629118332674e6484167967af53f1d2
Вадим <vkuprin017@gmail.com>
2026-04-09 02:02:44 +0300

    Add Card and HhData components, update helpers
    Introduce Card and HhData components with their props, styles and a rate SVG; HhData renders vacancy counts and salary tiers using Card and the new priceRu helper. Export new components from components/index. Update TopPageComponent to use Htag, Tag, HhData and render product list; add TopPageComponent styles. Minor CSS tweaks: Htag spacing, layout gaps/responsiveness and body padding.

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,10 +1,29 @@
 import { JSX } from 'react';
+import { HhData, Htag, Tag } from '../../components';
+import { TopLevelCategory } from '../../interfaces/page.interface';
+import styles from './TopPageComponent.module.css';
 import { TopPageComponentProps } from './TopPageComponent.props';
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
 	return (
-		<>
-			{products && products.length}
-		</>
+		<div className={styles.wrapper}>
+            <div className={styles.title}>
+                <Htag tag='h1'>{page.title}</Htag>
+                {products && <Tag color='grey' size='m'>{products.length}</Tag>}
+                <span>Сортировка</span>
+
+            </div>
+
+            <div>
+                {products && products.map(page => (<div key={page._id}>{page.title}</div>))}
+            </div>
+
+            <div className={styles.hhTitle}>
+                <Htag tag='h2'>Вакансии - {page.category}</Htag>
+                {products && <Tag color='red' size='m'>hh.ru</Tag>}
+            </div>
+
+            {firstCategory == TopLevelCategory.Courses && <HhData {...page.hh} />}
+		</div>
 	);
-};
+}; 
